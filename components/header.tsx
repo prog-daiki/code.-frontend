da67db9103aaeb8705d4e9f95f9d95b0fffb294e
prog-daiki <t.daiki0913@icloud.com>
2024-04-27 22:51:36 +0900

    ヘッダーの修正

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { Logo } from "@/components/logo";
 import { HeaderNav } from "@/components/header-nav";
 
 export const Header = () => {
   return (
-    <div className="h-16 container flex items-center justify-between border-b">
-      <Logo />
-      <HeaderNav />
-    </div>
+    <header className="border-b">
+      <div className="h-16 container flex items-center justify-between">
+        <Logo />
+        <HeaderNav />
+      </div>
+    </header>
   );
 };
