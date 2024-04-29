ca8da1f1e0ca271be6bc0ec405cf25ee75731834
prog-daiki <t.daiki0913@icloud.com>
2024-04-29 22:53:30 +0900

    callbackを修正

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "experimental-edge",
+};
+
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { type CookieOptions, createServerClient } from "@supabase/ssr";
