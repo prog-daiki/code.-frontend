71b732fc9d47c7b2fc382df42162ffbaa06509ed
prog-daiki <t.daiki0913@icloud.com>
2024-04-29 23:09:35 +0900

    not-foundページの作成

--- data/user.ts
+++ data/user.ts
@@ -5,7 +5,6 @@ export const getUser = async () => {
   const { data: user, error } = await supabase.auth.getUser();
 
   if (error) {
-    console.error("Error fetching user:", error.message);
     return null;
   }
 
